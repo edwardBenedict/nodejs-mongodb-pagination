87ba53c30df3b91f4f504f62c913b86b13656dc7
edwardBenedict <edwardbenedict92@gmail.com>
2021-06-26 20:53:06 +0300

    feat: add mongodb pagination

--- middilewares/pagination.js
+++ middilewares/pagination.js
@@ -1,4 +1,4 @@
-module.exports.paginatedResults = function (model) {
+module.exports.localPaginatedResults = function (model) {
   return (req, res, next) => {
     const page = parseInt(req.query.page);
     const limit = parseInt(req.query.limit);
@@ -25,6 +25,45 @@ module.exports.paginatedResults = function (model) {
     }
 
     result.results = model.slice(startIndex, endIndex);
+
+    res.paginatedResults = result;
+    next();
+  };
+};
+
+module.exports.MongoDBpaginatedResults = function (model) {
+  return async (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const result = {
+      status: 200,
+    };
+
+    if (endIndex < model.length) {
+      result.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      result.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+
+    try {
+      result.results = await model.find().limit(limit).skip(startIndex).exec();
+      res.paginatedResults = result;
+      next();
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
 
     res.paginatedResults = result;
     next();
--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 const express = require("express");
+require("dotenv").config();
 var pagination = require("./middilewares/pagination");
 var posts = require("./posts.json");
 var users = require("./users.json");
+const mongoose = require("mongoose");
+const Comment = require("./model/comments");
 
 const app = express();
 
-app.get("/posts", pagination.paginatedResults(posts), (req, res) => {
+DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@trainingcluster.tpyei.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose
+  .connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB!"))
+  .catch((error) => console.log("Error : ", error));
+
+app.get("/posts", pagination.localPaginatedResults(posts), (req, res) => {
   res.json(res.paginatedResults);
 });
 
-app.get("/users", pagination.paginatedResults(users), (req, res) => {
+app.get("/users", pagination.localPaginatedResults(users), (req, res) => {
   res.json(res.paginatedResults);
 });
+
+app.get(
+  "/comments",
+  pagination.MongoDBpaginatedResults(Comment),
+  (req, res) => {
+    res.json(res.paginatedResults);
+  }
+);
 
 app.listen(3000);
